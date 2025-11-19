e7a024bff9cf0181a2195fc8ede48a7701b72b57
Per Sefastsson <per@stockholmsschakt.se>
2025-11-19 14:43:52 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#fd6201',
+    },
+    secondary: {
+      main: '#019cfd',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+};
